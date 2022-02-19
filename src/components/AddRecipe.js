9005de5f2ce6983e030264708455b32a7cab4428
dean <diazdean21@gmail.com>
2022-02-19 14:14:34 -0600

    made components

---
+++ src/components/AddRecipe.js
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+
+class AddRecipe extends Component{
+  constructor(props) {
+    super(props)
+    this.state= {
+      fullName: null
+
+    }
+  }
+
+handleSubmit =(event) => {
+  event.preventDefault()
+}
+  handleInputChange = (event) => {
+    event.preventDefault()
+    console.log(event.target.name);
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  render () {
+    const {fullName} = this.state
+    return (
+      <div>
+        <h1>New Recipe</h1>
+        <p>Full name is:{fullName}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p><input type ='text' placeholder="Your Name" name='fullName' onChange={handleInputChange}/></p>
+          <p><input type='text' placeholder='New Recipe'/></p>
+          <button>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+
+
+export default AddRecipe;
